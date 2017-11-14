6db9cc4aeed9339cac7769c30993c8fe41904beb
AkshayCHD <akshaykumarsharma1998aks@gmail.com>
2017-11-15 01:27:44 +0530

    functional search

--- app/src/main/java/uietdig/volleydemo/MainActivity.java
+++ app/src/main/java/uietdig/volleydemo/MainActivity.java
@@ -39,34 +39,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mContext = this;
 
-//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, SERVER_URL+"typhoon attack", null,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        Log.v("MainActivity", "testingServerSuccessResponse: "+ response);
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.v("MainActivity", "testingServerErrorResponse: "+ error);
-//            }
-//        }){
-//            @Override
-//            public Map<String, String> getHeaders() throws AuthFailureError {
-//                try {
-//                    Map<String, String> headers = new HashMap<>();
-//                    String key = "Authorization";
-//                    headers.put(key, "Token " + "47544bd17b86dfd574d6a1eff843d0dfaa7a2a88");
-//                    return headers;
-//                } catch (Exception e) {
-//                }
-//                return super.getParams();
-//            }
-//        };
-//        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
-//        requestQueue.add(jsonObjectRequest);
 
-        HashMap<String, String > contactDetails = new HashMap();
+      /*HashMap<String, String > contactDetails = new HashMap();
         contactDetails.put("mobileNum", "9876543210");
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PATCH, USER_DETAILS_ENDPOINT,
@@ -101,11 +75,11 @@ public class MainActivity extends AppCompatActivity {
         RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
         jsonObjectRequest.setRetryPolicy(policy);
         RequestQueue requestQueue =  Volley.newRequestQueue(mContext);
-        requestQueue.add(jsonObjectRequest);
+        requestQueue.add(jsonObjectRequest);*/
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
 
         MenuItem item = menu.findItem(R.id.action_search);
         searchView = (MaterialSearchView) findViewById(R.id.search_view);
@@ -113,6 +87,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 //Do some magic
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, SERVER_URL+query, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.v("MainActivity", "testingServerSuccessResponse: "+ response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("MainActivity", "testingServerErrorResponse: "+ error);
+                    }
+                }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        try {
+                            Map<String, String> headers = new HashMap<>();
+                            String key = "Authorization";
+                            headers.put(key, "Token " + "2bf1fb77bb3f7b1c1085f85bbe2982ef172c453e");
+                            return headers;
+                        } catch (Exception e) {
+                        }
+                        return super.getParams();
+                    }
+                };
+                RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+                requestQueue.add(jsonObjectRequest);
+
                 return false;
             }
 
