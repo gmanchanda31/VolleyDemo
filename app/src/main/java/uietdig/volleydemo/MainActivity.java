11f0da415974751e0c221b567b335eae4398900c
AkshayCHD <akshaykumarsharma1998aks@gmail.com>
2017-11-15 01:52:21 +0530

    can change number

--- app/src/main/java/uietdig/volleydemo/MainActivity.java
+++ app/src/main/java/uietdig/volleydemo/MainActivity.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
@@ -38,45 +42,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = this;
-
-
-      /*HashMap<String, String > contactDetails = new HashMap();
-        contactDetails.put("mobileNum", "9876543210");
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PATCH, USER_DETAILS_ENDPOINT,
-                new JSONObject(contactDetails),
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.v("MainActivity", "testingServerSuccessResponse: "+ response);
-
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.v("MainActivity", "testingServerErrorResponse: "+ error);
-
-            }
-        }){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                try {
-                    Map<String, String> headers = new HashMap<>();
-                    String key = "Authorization";
-                    headers.put(key, "Token " + "2bf1fb77bb3f7b1c1085f85bbe2982ef172c453e");
-                    return headers;
-                } catch (Exception e) {
-                }
-                return super.getParams();
-            }
-        };
-        int socketTimeout = 50000;//30 seconds - change to what you want
-        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-        jsonObjectRequest.setRetryPolicy(policy);
-        RequestQueue requestQueue =  Volley.newRequestQueue(mContext);
-        requestQueue.add(jsonObjectRequest);*/
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -91,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
                         new Response.Listener<JSONObject>() {
                             @Override
                             public void onResponse(JSONObject response) {
-                                Log.v("MainActivity", "testingServerSuccessResponse: "+ response);
+                                Toast.makeText(mContext,"testingServerSuccessResponse: "+ response, Toast.LENGTH_SHORT).show();
+                                Log.v("MainActivity", "testingServerErrorResponse: "+ response);
                             }
                         }, new Response.ErrorListener() {
                     @Override
@@ -155,4 +123,48 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    public void changeNumber(View view) {
+        EditText NumberChangeEditText = (EditText) findViewById(R.id.numberchangeditText);
+        HashMap<String, String > contactDetails = new HashMap();
+        if(NumberChangeEditText.getText().toString().length() == 10) {
+            contactDetails.put("mobileNum", NumberChangeEditText.getText().toString());
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PATCH, USER_DETAILS_ENDPOINT,
+                    new JSONObject(contactDetails),
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Toast.makeText(mContext, "testingServerSuccessResponse: " + response,Toast.LENGTH_SHORT).show();
+
+                            Log.v("MainActivity", "testingServerErrorResponse: " + response);
+
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.v("MainActivity", "testingServerErrorResponse: " + error);
+
+                }
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    try {
+                        Map<String, String> headers = new HashMap<>();
+                        String key = "Authorization";
+                        headers.put(key, "Token " + "2bf1fb77bb3f7b1c1085f85bbe2982ef172c453e");
+                        return headers;
+                    } catch (Exception e) {
+                    }
+                    return super.getParams();
+                }
+            };
+            int socketTimeout = 50000;//30 seconds - change to what you want
+            RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+            jsonObjectRequest.setRetryPolicy(policy);
+            RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+            requestQueue.add(jsonObjectRequest);
+        }else
+            Toast.makeText(mContext, "The number entered is invalid", Toast.LENGTH_SHORT).show();
+    }
 }
